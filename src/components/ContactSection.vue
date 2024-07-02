33162801c4e5e28874835093605496010037e8f7
L O U I S <luis_henano@hotmail.com>
2024-07-02 01:45:09 -0300

    Feat: Contact Section

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-5xl font-bold text-white mb-6">Let's connect!</div>
+  <div class="text-2xl font-bold text-white">
+    Have any questions or just want to say hello? get in touch with me via social media or email.
+  </div>
+</template>
