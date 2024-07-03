277803babe52a1f8731e487b7ed4cf81f72dde7f
L O U I S <luis_henano@hotmail.com>
2024-07-02 22:21:51 -0300

    Feat: Added social icons to footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="container w-full md:max-w-3xl mx-auto">
     <div class="text-slate-300 flex space-x-4 justify-center">
-      <!-- <a
-          href="{link.url}"
-          target="_blank"
-          rel="noreferrer"
-          class="icon hover:text-white transition-transform duration-300 hover:scale-125"
-        >
-          {#if link.text === 'Github'}
-          <FaGithub />
-          {/if} {#if link.text === 'LinkedIn'}
-          <FaLinkedin />
-          {/if} {#if link.text === 'Instagram'}
-          <FaInstagram />
-          {/if} {#if link.text === 'Facebook'}
-          <FaFacebookSquare />
-          {/if}
-        </a> -->
+      <a
+        v-for="(item, index) in socialLinks"
+        :key="index"
+        :href="item.link"
+        target="_blank"
+        class="hover:text-white transition-transform duration-300 hover:scale-125"
+      >
+        <SocialIcon :title="item.title" />
+      </a>
     </div>
     <p class="text-slate-700 flex justify-center my-10">Made with ❤️ and 🎵 by Louie</p>
     <p class="text-slate-700 flex justify-center my-10">Copyright {{ date.getFullYear() }}</p>
   </div>
 </template>
 <script setup>
+import SocialIcon from './SocialIcon.vue';
+
 const date = new Date();
+
+const socialLinks = [
+  {
+    title: 'instagram',
+    link: 'https://instagram.com/louiewuzhere'
+  },
+  {
+    title: 'github',
+    link: 'https://github.com/louiecodes'
+  },
+  {
+    title: 'linkedin',
+    link: '#!'
+  }
+];
 </script>
---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <component :is="icon" class="icon" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import InstagramIcon from './icons/InstagramIcon.vue';
+import GithubIcon from './icons/GithubIcon.vue';
+import LinkedInIcon from './icons/LinkedInIcon.vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+});
+
+const icon = computed(() => {
+  switch (props.title) {
+    case 'instagram':
+      return InstagramIcon;
+    case 'github':
+      return GithubIcon;
+    case 'linkedin':
+      return LinkedInIcon;
+    default:
+      return null;
+  }
+});
+</script>
+
+<style scoped>
+.icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
