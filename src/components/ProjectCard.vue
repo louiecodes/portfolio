5b1856629d3e80156694498b6c05f4f52955fb91
L O U I S <luis_henano@hotmail.com>
2024-07-04 18:33:22 -0300

    Style: Added animations to project cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,7 +2,10 @@
   <!-- 
     class="rounded-xl shadow-lg transition-all duration-300 bg-gradient-to-br from-emerald-200 via-amber-100 to-rose-400 hover:bg-transparent" 
   -->
-  <div class="rounded-xl shadow-lg transition-all duration-300" :class="project.background">
+  <div
+    class="rounded-xl shadow-lg transition-all duration-300 group border border-transparent hover:from-black hover:to-black hover:border hover:border-white"
+    :class="project.background"
+  >
     <div class="flex justify-end px-4 pt-4">
       <button class="inline-block text-gray-500 text-sm p-1.5" type="button">
         <span class="sr-only">Open link</span>
@@ -21,18 +24,22 @@
     </div>
     <div class="p-6 flex flex-col h-full">
       <div class="text-center mb-6">
-        <div class="text-3xl font-black text-black transition duration-300 hover:text-slate-50">
+        <div
+          class="text-3xl font-black text-black transition duration-300 translate-y-full group-hover:-translate-y-0 group-hover:text-slate-50"
+        >
           <a :href="project.link" target="_blank">{{ project.title }}</a>
         </div>
       </div>
-      <div class="text-center text-gray-500 whitespace-pre-line mb-6">
+      <div
+        class="text-center text-gray-400 mb-10 transition duration-300 opacity-0 -translate-y-10 group-hover:translate-y-0 group-hover:opacity-100"
+      >
         {{ project.description }}
       </div>
       <div class="flex flex-wrap mt-auto">
         <div
           v-for="(item, index) in project.technologies"
           v-bind:key="index"
-          class="text-black font-semibold me-4"
+          class="text-black font-semibold me-4 group-hover:text-white"
         >
           {{ item }}
         </div>
