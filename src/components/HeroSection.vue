2ebadd0636286ebc4bc4a7d43585e9769611cb3d
L O U I S <luis_henano@hotmail.com>
2024-07-04 11:39:24 -0300

    Fix: Animation delay in hero section and contact button

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -16,20 +16,20 @@
 
     <div
       v-motion-pop-visible-once
-      :delay="4000"
+      :delay="3000"
       :duration="500"
       class="relative inline-flex group mt-16"
     >
       <div
         class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200"
       />
-      <a
-        href="#contact"
+      <router-link
+        to="#contact"
         role="button"
         class="relative inline-flex items-center justify-center px-8 py-4 text-2xl font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 rounded-xl"
       >
         Get in touch
-      </a>
+      </router-link>
     </div>
   </div>
 </template>
