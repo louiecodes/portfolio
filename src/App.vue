1cb2c3af885cc326b8fa720c5b5cefdb12a079a6
L O U I S <luis_henano@hotmail.com>
2025-07-15 06:47:43 -0300

    style: Refactor layout structure and enhance background styling

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,31 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container w-full md:max-w-3xl mx-auto px-4 sm:px-4 md:px-4 lg:px-0">
-    <header>
-      <NavbarComponent />
-    </header>
-    <main>
-      <RouterView />
-    </main>
-    <footer class="pt-40">
-      <FooterComponent />
-    </footer>
+  <div class="min-h-screen w-full bg">
+    <div class="container w-full md:max-w-3xl mx-auto px-4 sm:px-4 md:px-4 lg:px-0">
+      <header>
+        <NavbarComponent />
+      </header>
+      <main>
+        <RouterView />
+      </main>
+      <footer class="pt-40">
+        <FooterComponent />
+      </footer>
+    </div>
   </div>
 </template>
 <style>
 body {
   background-color: #000;
+}
+
+.bg {
+  background-image:
+  radial-gradient(circle 800px at 0% 100%, rgba(139, 92, 246, 0.3), transparent),
+  radial-gradient(circle 800px at 100% 10%, rgba(59, 130, 246, 0.3), transparent);
+  background-size: 100% 100%, 100% 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
