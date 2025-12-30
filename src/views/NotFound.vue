ecd16f21fcbd0fe4944d82f77ea86de8652ba366
L O U I S <luis_henano@hotmail.com>
2025-12-30 07:03:46 -0300

    feat: Added glitch animation to Not Found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,11 @@
 <template>
   <section class="min-h-screen flex justify-center items-center">
     <div class="text-white text-center">
-      <h1 class="text-4xl md:text-5xl font-bold mb-12">{{ randomErrorMessage }}</h1>
-
+      <h1
+        class="glitch-text animate-[glitch_1s_infinite] mx-auto text-4xl md:text-5xl font-bold mb-12"
+      >
+        {{ randomErrorMessage }}
+      </h1>
       <router-link
         to="/"
         class="relative inline-flex items-center justify-center px-8 py-4 text-xl font-bold text-white transition-all duration-200 bg-gray-900 hover:bg-gray-800 border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 rounded-xl"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
-import flowbite from 'flowbite/plugin'
+import flowbite from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
-      'animation': {
-        'text': 'text 5s ease infinite',
+      animation: {
+        text: 'text 5s ease infinite'
       },
-      'keyframes': {
+      keyframes: {
         text: {
           '0%': {
             backgroundSize: '300%',
@@ -26,11 +22,36 @@ export default {
             backgroundSize: '300%',
             backgroundPosition: '0% 50%'
           }
+        },
+        glitch: {
+          '0%': {
+            color: '#fff',
+            textShadow: '1px 1px 0px #00ffff, -1px -1px 0px #ff00ff'
+          },
+          '25%': {
+            color: '#fff',
+            textShadow: '-1px -1px 0px #fff, 1px 1px 0px #ff00ff'
+          },
+          '50%': {
+            color: '#fff',
+            textShadow: '1px -1px 0px #00ffff, -1px 1px 0px #fff'
+          },
+          '75%': {
+            color: '#fff',
+            textShadow: '-1px 1px 0px #ff00ff, 1px -1px 0px #00ffff'
+          },
+          '100%': {
+            color: '#fff',
+            textShadow: '1px 1px 0px #00ffff, -1px -1px 0px #ff00ff'
+          }
+        },
+        twinkle: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '0' }
         }
       }
-    },
+    }
   },
-  plugins: [
-    flowbite,
-  ],
-}
+  plugins: [flowbite]
+};
